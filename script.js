337b6e4aa4cc798202002c993d7c3d3b53f97f99
KaranMalviya <karanmalviya258@gmail.com>
2022-07-09 15:39:42 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-fetch("https://icanhazdadjoke.com/slack")
-  .then((data) => data.json())
-  .then((jokeData) => {
-    const jokeText = jokeData.attachments[0].text;
-    const jokeElement = document.getElementById("jokeElement");
+document.getElementById("myButton").addEventListener("click", meme);
+function meme() {
+  fetch("https://icanhazdadjoke.com/slack")
+    .then((data) => data.json())
+    .then((jokeData) => {
+      const jokeText = jokeData.attachments[0].text;
+      const jokeElement = document.getElementById("jokeElement");
 
-    jokeElement.innerHTML = jokeText;
-  });
+      jokeElement.innerHTML = jokeText;
+    });
+}
+window.onload = meme;
