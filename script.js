85bec1636e5b3f8fb0867a5c1bc948ed249709ab
KaranMalviya <karanmalviya258@gmail.com>
2022-07-16 21:37:34 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-document.getElementById("myButton").addEventListener("click", meme);
-function meme() {
-  fetch("https://icanhazdadjoke.com/slack")
-    .then((data) => data.json())
-    .then((jokeData) => {
-      const jokeText = jokeData.attachments[0].text;
-      const jokeElement = document.getElementById("jokeElement");
+// Importing CSS in JS File
+var link = document.createElement("link");
+// set the attributes for link element
+link.rel = "stylesheet";
+link.type = "text/css";
+link.href = "style.css";
 
-      jokeElement.innerHTML = jokeText;
-    });
+// Get HTML head element to append
+// link element to it
+document.getElementsByTagName("HEAD")[0].appendChild(link);
+
+document.querySelector("#myButton").addEventListener("click", meme);
+function meme() {
+  fetch("https://meme-api.herokuapp.com/gimme")
+    .then((response) => response.json())
+    .then((jokeData) => {
+      const img = jokeData.url;
+      const author = jokeData.author;
+      const memeElement = document.querySelector("#jokeElement");
+      // const img = document.createElement("img")
+      memeElement.innerHTML = `<div>
+                              <img src="${img}" class="meme_img"/>
+                              <p class="author-text">Author:&nbsp${author}</p>
+                              </div>`;
+    })
+    .catch((err) => console.error(err));
 }
 window.onload = meme;
